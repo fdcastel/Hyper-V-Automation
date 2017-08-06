437c28eac9678b5dddd3271b5cee2f6457d56df7
F.D.Castel <fdcastel@gmail.com>
2017-08-06 07:26:43 -0300

    Fix unattend file for Windows 10 (unlocks administrator user, minor improvements).

--- New-WindowsUnattendFile.ps1
+++ New-WindowsUnattendFile.ps1
@@ -24,16 +24,36 @@ $template = @'
             <ProductKey></ProductKey>
             <ComputerName></ComputerName>
         </component>
-    </settings>
-    <settings pass="oobeSystem">
         <component name="Microsoft-Windows-International-Core" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <InputLocale>en-US</InputLocale>
             <SystemLocale>en-US</SystemLocale>
             <UserLocale>en-US</UserLocale>
         </component>
+        <component name="Microsoft-Windows-Security-SPP-UX" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <SkipAutoActivation>true</SkipAutoActivation>
+        </component>
+        <component name="Microsoft-Windows-SQMApi" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <CEIPEnabled>0</CEIPEnabled>
+        </component>
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="x86" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Order>1</Order>
+                    <Path>net user administrator /active:yes</Path>
+                </RunSynchronousCommand>
+            </RunSynchronous>
+        </component>
+    </settings>
+    <settings pass="oobeSystem">
         <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
             <OOBE>
                 <HideEULAPage>true</HideEULAPage>
+                <HideLocalAccountScreen>true</HideLocalAccountScreen>
+                <HideOEMRegistrationScreen>true</HideOEMRegistrationScreen>
+                <HideOnlineAccountScreens>true</HideOnlineAccountScreens>
+                <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
+                <SkipMachineOOBE>true</SkipMachineOOBE>
+                <SkipUserOOBE>true</SkipUserOOBE>
             </OOBE>
             <UserAccounts>
                 <AdministratorPassword>
@@ -53,13 +73,13 @@ if (-not $FilePath) {
 }
 
 if ($ComputerName) {
-    $xml.unattend.settings[0].component.ComputerName = $ComputerName
+    $xml.unattend.settings[0].component[0].ComputerName = $ComputerName
 }
 
 if ($Locale) {
-    $xml.unattend.settings[1].component[0].InputLocale = $Locale
-    $xml.unattend.settings[1].component[0].SystemLocale = $Locale
-    $xml.unattend.settings[1].component[0].UserLocale = $Locale
+    $xml.unattend.settings[0].component[1].InputLocale = $Locale
+    $xml.unattend.settings[0].component[1].SystemLocale = $Locale
+    $xml.unattend.settings[0].component[1].UserLocale = $Locale
 }
 
 # Source: https://technet.microsoft.com/en-us/library/jj612867(v=ws.11).aspx
@@ -69,10 +89,10 @@ $key = switch ($Version){
     'Windows10Enterprise'   {'NPPR9-FWDCX-D2C8J-H872K-2YT43'}
     'Windows10Professional' {'W269N-WFGWX-YVC9B-4J6C9-T83GX'}
 }
-$xml.unattend.settings[0].component.ProductKey = $key
+$xml.unattend.settings[0].component[0].ProductKey = $key
 
 $encodedPassword = [System.Text.Encoding]::Unicode.GetBytes($AdministratorPassword + 'AdministratorPassword')
-$xml.unattend.settings[1].component[1].UserAccounts.AdministratorPassword.Value = [Convert]::ToBase64String($encodedPassword)
+$xml.unattend.settings[1].component.UserAccounts.AdministratorPassword.Value = [Convert]::ToBase64String($encodedPassword)
 
 $writer = New-Object System.XMl.XmlTextWriter($FilePath, [System.Text.Encoding]::UTF8)
 $writer.Formatting = [System.Xml.Formatting]::Indented
