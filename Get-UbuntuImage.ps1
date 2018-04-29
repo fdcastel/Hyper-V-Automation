bd947e18d94134f24ff910af0e93303af7104f7e
F.D.Castel <fdcastel@gmail.com>
2018-04-28 23:36:47 -0300

    Get-UbuntuImage.ps1: Adds support to Ubuntu 18.04 LTS.

--- Get-UbuntuImage.ps1
+++ Get-UbuntuImage.ps1
@@ -1,18 +1,28 @@
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$OutFileName
+    [string]$OutFileName,
+    
+    [switch]$Previous
 )
 
 $ErrorActionPreference = 'Stop'
 
 # Note: Github removed TLS 1.0 support. Enables TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol-bor 'Tls12'
+# [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol-bor 'Tls12'
 
-$url = 'https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-uefi1.img'
+$urlRoot = 'https://cloud-images.ubuntu.com/releases/18.04/release'
+$urlFile = 'ubuntu-18.04-server-cloudimg-amd64.img'
+
+if ($Previous) {
+    $urlRoot = 'https://cloud-images.ubuntu.com/releases/16.04/release'
+    $urlFile = 'ubuntu-16.04-server-cloudimg-amd64-uefi1.img'
+}
+
+$url = "$urlRoot/$urlFile"
+        
 $imgFile = $OutFileName
 if (-not $imgFile) {
-    $imgFile = '.\ubuntu-16.04-server-cloudimg-amd64-uefi1.img'
+    $imgFile = ".\$urlFile"
 }
 
 $client = New-Object System.Net.WebClient
@@ -20,8 +30,9 @@ $client.DownloadFile($url, $imgFile)
 
 # Check file integrity
 $sha1Hash = Get-FileHash $imgFile -Algorithm SHA1
- $allHashs = $client.DownloadString('https://cloud-images.ubuntu.com/releases/16.04/release/SHA1SUMS')
-$m = [regex]::Matches($allHashs, '(?<Hash>\w{40})\s\*ubuntu-16.04-server-cloudimg-amd64-uefi1.img')
+$allHashs = $client.DownloadString("$urlRoot/SHA1SUMS")
+$m = [regex]::Matches($allHashs, "(?<Hash>\w{40})\s\*$urlFile")
+if (-not $m[0]) { throw "Cannot get SHA1 hash for $urlFile." }
 $expectedHash = $m[0].Groups['Hash'].Value
 if ($sha1Hash.Hash -ne $expectedHash) { throw 'Integrity check failed.' }
 
