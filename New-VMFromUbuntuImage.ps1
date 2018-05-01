d413b4f1a150190f09a9cf2430a12e0ba1e7aff4
F.D.Castel <fdcastel@gmail.com>
2018-05-01 15:15:40 -0300

    Revert docker installation to docker-ce.

--- New-VMFromUbuntuImage.ps1
+++ New-VMFromUbuntuImage.ps1
@@ -70,7 +70,12 @@ ssh_authorized_keys:
     if ($InstallDocker) {
         $sectionRunCmd += @'
 
- - 'apt install docker.io docker-compose -y'
+ - 'apt install -y apt-transport-https ca-certificates curl software-properties-common'
+ - 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+ - 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+ - 'apt update'
+ - 'apt install -y docker-ce'
+ - 'apt install -y docker-compose'
 '@
     }
 
