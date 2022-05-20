402dcc58464bc1fc3ab71523ed35949ac95008e9
F.D.Castel <fdcastel@gmail.com>
2022-05-20 17:51:32 -0300

    Updates docker installation with latest docs (https://docs.docker.com/engine/install/ubuntu/).

--- New-VMFromUbuntuImage.ps1
+++ New-VMFromUbuntuImage.ps1
@@ -345,9 +345,9 @@ ssh_authorized_keys:
 if ($InstallDocker) {
     $sectionRunCmd += @'
 
- - 'apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common'
- - 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
- - 'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+ - 'apt install -y apt-transport-https ca-certificates curl gnupg lsb-release'
+ - 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
+ - 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
  - 'apt update -y'
  - 'apt install -y docker-ce docker-ce-cli containerd.io docker-compose'
 '@
