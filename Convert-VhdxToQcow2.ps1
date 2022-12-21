f1e9ab5d50b3ff210ad63b63c02e008bc7b82566
F.D.Castel <fdcastel@gmail.com>
2022-12-21 12:46:22 -0300

    Convert-VhdxToQcow2.ps1: Fix output streams. Adds support for -Verbose.

--- Convert-VhdxToQcow2.ps1
+++ Convert-VhdxToQcow2.ps1
@@ -12,7 +12,7 @@ if (-not $TargetQcow2) {
     $TargetQcow2 = [io.path]::ChangeExtension($SourceVhdx, '.qcow2')
 }
 
-& qemu-img.exe convert -p -f vhdx -O qcow2 $SourceVhdx $TargetQcow2
+& qemu-img.exe convert -p -f vhdx -O qcow2 $SourceVhdx $TargetQcow2 2>&1 | Write-Verbose
 if ($LASTEXITCODE -ne 0) {
     throw "qemu-img returned $LASTEXITCODE. Aborting."
 }
